1761b2dd7d55fa7d22cca3abb8a7cdc739f29158
Jack Rimell <jack.rimell@sonovate.com>
2022-04-19 17:06:57 +0100

    refactor: remove optional options object for nextJest

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,11 @@
+const nextJest = require('next/jest');
 
-const nextJest = require('next/jest')
-
-const createJestConfig = nextJest({
-  dir: './',
-})
+const createJestConfig = nextJest();
 
 const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['./jest.setup.ts'],
-}
+};
 
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
